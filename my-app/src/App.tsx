75b7f0f8bfa00069f04ab62733bef7e162e433bf
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-08-12 09:49:08 -0300

    feat: Iniciando com git flow + Componentização em Vite+React.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+
+
+//Vamos aprender a criar um componente funcional
+
+import Alexandre from "./components/Nome/Alexandre";
+
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>Olá Mundo!</h1>
+      <Alexandre/>
+    </div>
+  );
+  
+} 
---
+++ my-app/src/components/Nome/Alexandre.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Alexandre() {
+    return(
+        <div>
+            <h2>Alexandre</h2>
+        </div>
+
+    );
+}
