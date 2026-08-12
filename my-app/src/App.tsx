4c01388a8c184b8e80b731d0d7cd4282e023ab9a
Gizetto61 <gigiozetto@gmail.com>
2026-08-12 09:49:05 -0300

    feat: Iniciando com git flow + Componentização em Vite + React.

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+// Vamos aprender a criar um componente funcional em react
+
+import Giovanni from "./components/Nome/Giovanni";
+
+export default function App() {
+  
+  return (
+  <div>
+    <h1>Olá, Mundo!</h1>
+    <Giovanni/>
+  </div>
+);
+
+}
---
+++ my-app/src/components/Nome/Giovanni.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Giovanni() {
+    
+    return (
+        <div>
+            <h2>Giovanni</h2>
+        </div>
+    );
+
+}
