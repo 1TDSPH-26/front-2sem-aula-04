328f1dee2fb6262b94c353331c4a102e0ccc9738
Luigi Tormim <luiginhotoca@gmail.com>
2026-08-12 09:48:46 -0300

    feat: iniciando com git flow + Componentizacao com Vite+React

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+//vamos aprender a criar um componente funcional
+
+import Luigi from "./componentes/Nome/Luigi";
+
+export default function App () {
+
+  return (
+  <div>
+    <h1>Ola mundo</h1>
+    <Luigi/>
+    </div>
+    
+  );
+}
