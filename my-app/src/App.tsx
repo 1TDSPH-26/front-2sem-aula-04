048806cc824b5756566bf490108614365b3cf0e3
Luigi Tormim <luiginhotoca@gmail.com>
2026-08-12 09:53:44 -0300

    feat: arrumando o merge q dei ssem querer

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 //vamos aprender a criar um componente funcional
 
 import Luigi from "./componentes/Nome/Luigi";
@@ -13,22 +13,3 @@ export default function App () {
     
   );
 }
-=======
-
-
-//Vamos aprender a criar um componente funcional
-
-import Alexandre from "./components/Nome/Alexandre";
-
-
-export default function App() {
-
-  return (
-    <div>
-      <h1>Olá Mundo!</h1>
-      <Alexandre/>
-    </div>
-  );
-  
-} 
->>>>>>> 75b7f0f8bfa00069f04ab62733bef7e162e433bf
